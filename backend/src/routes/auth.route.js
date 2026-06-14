e8cb6ef636d490ee95e02d193fd8fa2afac1edea
Raunak917 <2300520400051@ietlucknow.ac.in>
2026-06-14 17:04:39 +0530

    project completed RConnect

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { checkAuth, logIn, logOut, signUp, updateProfile } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middlewares/auth.middleware.js';
+
+const router = express.Router();
+
+router.post("/login", logIn);
+
+router.post("/signup", signUp);
+
+router.post("/logout", logOut);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
+
+export default router;
+
