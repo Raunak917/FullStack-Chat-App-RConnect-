4877025bbfbb5e5c0c7fba01771a7f8d491243f4
Raunak917 <2300520400051@ietlucknow.ac.in>
2026-06-14 23:43:59 +0530

    Fix frontend dist path

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,10 +26,12 @@ app.use("/api/auth", userRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+    res.sendFile(
+      path.join(__dirname, "../frontend/dist/index.html")
+    );
   });
 }
 
