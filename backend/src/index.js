31c8272ab99b782e5e58e132aa70910a3c63c02c
Raunak917 <2300520400051@ietlucknow.ac.in>
2026-06-14 17:54:14 +0530

    .env changes done

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ app.use("/api/auth", userRoutes);
 
 app.use("/api/messages", messageRoutes);
 
-if(process.env.MODE_ENV==="production"){
+if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req,res)=>{
